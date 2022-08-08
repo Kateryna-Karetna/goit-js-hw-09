1a6798ce72f1dc928b142cec76d8782df1b94f91
Kateryna-Karetna <98160697+Kateryna-Karetna@users.noreply.github.com>
2022-08-08 09:03:20 +0000

    Deploying to gh-pages from @ Kateryna-Karetna/goit-js-hw-09@105b3a277cfff44b262613876068d736edc234eb 🚀

---
+++ 03-promises.58c13e21.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("input",(function(t){"amount"===t.target.name&&console.log(t.target.value)}));function t(t,e){Math.random()}document.querySelector("button").addEventListener("submit",t);for(let e=0;e<5;e++)t();
+//# sourceMappingURL=03-promises.58c13e21.js.map
---
+++ 03-promises.e5b76be3.js
@@ -0,0 +1,2 @@
+!function(){function t(t,e){Math.random()}document.querySelector(".form").addEventListener("input",(function(t){"amount"===t.target.name&&console.log(t.target.value)})),document.querySelector("button").addEventListener("submit",t);for(var e=0;e<5;e++)t()}();
+//# sourceMappingURL=03-promises.e5b76be3.js.map
